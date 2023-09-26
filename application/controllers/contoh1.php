66f5fdd3205b45046ad39553f90ea46fb30d00eb
chairul <chairulpratama8602@gmail.com>
2023-09-26 16:44:47 +0700

    menambahkan control contoh1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Chairul Pratama, saya tinggal di jakarta, dan mempunyai hobi menabung";
+    
+    }
+}
